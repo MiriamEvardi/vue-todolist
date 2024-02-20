555e17fd0fd2f89297913b87ec3b9175721bce01
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-20 15:30:36 +0100

    base layout

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@ createApp({
     data() {
         return {
 
-
+            toDoList: [
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+                {
+                    text: "Studiare JavaScript",
+                    done: true
+                },
+                {
+                    text: "Fare esercizio fisico",
+                    done: false
+                }
+            ]
 
         }
     },
